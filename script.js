18493e6dff14d9f0bb761cde73eace1797d20bd2
Wanderley <davidgomeswanderley@gmail.com>
2025-07-15 21:08:54 -0300

    criando arquivo css e modificando o index

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-  const uri =
-    "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1";
+const uri = "https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1";
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+     },
+};
 
-    },
-  };
+fetch(uri, options)
+  .then((res) => res.json())
+  .then((data) => {
+    const movies = data.results;
+    const container = document.getElementById("movies-container");
 
-  fetch(uri, options)
-    .then((res) => res.json())
-    .then((data) => {
-      const movies = data.results;
-      const container = document.getElementById("movies-container");
+    movies.forEach((movie) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.style.width = "18rem";
 
-      movies.forEach((movie) => {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.style.width = "18rem";
-
-        card.innerHTML = `
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />
+      card.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w500${
+            movie.poster_path
+          }" class="card-img-top" alt="${movie.title}" />
           <div class="card-body">
             <h5 class="card-title">${movie.title}</h5>
             <p class="card-text">${movie.overview.substring(0, 100)}...</p>
@@ -29,7 +29,7 @@
           </div>
         `;
 
-        container.appendChild(card);
-      });
-    })
-    .catch((err) => console.error(err));
+      container.appendChild(card);
+    });
+  })
+  .catch((err) => console.error(err));
